b45314f2aba647de121bfa3609b449b6440be3c6
jhanniellymarques <jhanniellymarques@hotmail.com>
2017-05-17 11:08:20 -0300

    Update ExemplogitNetbeans.java

--- src/exemplogit/netbeans/ExemplogitNetbeans.java
+++ src/exemplogit/netbeans/ExemplogitNetbeans.java
@@ -16,6 +16,8 @@ public class ExemplogitNetbeans {
      */
     public static void main(String[] args) {
         System.out.println("Exemplo de um projeto qualquer com git e netbeans ");
+    //alterado da web
     }
+    
     
 }
